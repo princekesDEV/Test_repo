a9ee37c68ac4864325df5c075f73a41fb4ffc805
princekesDEV <kesprince1250@gmail.com>
2024-01-30 11:43:07 +0530

    some more test files

---
+++ array.js
@@ -0,0 +1,4 @@
+let arr =["Prince","Priyansh","Harsh","Sam"];
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
---
+++ some.js
@@ -0,0 +1,6 @@
+let obj = {
+  name: "Prince",
+  age: 18,
+  address: "Shankargarh",
+  phone: "main Nahi Batauga",
+};
