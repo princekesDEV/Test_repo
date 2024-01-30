56b8e0cb0bbad1021bc6108d5ff6aedc8cf5a175
princekesDEV <127650353+princekesDEV@users.noreply.github.com>
2024-01-30 11:55:44 +0530

    Added more keys in obj

--- some.js
+++ some.js
@@ -3,6 +3,8 @@ let obj = {
   age: 18,
   address: "Shankargarh",
   phone: "main Nahi Batauga",
+  ipl_team:"Chennai Super Kings",
+  favourite_player: "Mahendra Singh Dhoni",
 };
 for (let i in obj) {
   console.log(i, ":-", obj[i]);
